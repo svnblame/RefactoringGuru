6bd61324969dff255934bac55740044e492b3fbd
Gene Kelley <phpcodehead@gmail.com>
2023-04-20 11:20:47 -0500

    Add Abstract Factory Real World example.

--- Creational/AbstractFactory/RealWorld/index.php
+++ Creational/AbstractFactory/RealWorld/index.php
@@ -105,3 +105,143 @@ interface PageTemplate
  * to set it in the sub-template object. The abstract factory will link the page
  * template with a title template of the same variant.
  */
+abstract class BasePageTemplate implements PageTemplate 
+{
+    protected $titleTemplate;
+
+    public function __construct(TitleTemplate $titleTemplate)
+    {
+        $this->titleTemplate = $titleTemplate;
+    }
+}
+
+/**
+ * The Twig variant of the whole page templates.
+ */
+class TwigPageTemplate extends BasePageTemplate
+{
+    public function getTemplateString(): string
+    {
+        $renderedTitle = $this->titleTemplate->getTemplateString();
+
+        return <<<HTML
+        <div class="page">
+            $renderedTitle
+            <article class="content">{{ content }}</article>
+        </div>
+        HTML;
+    }
+}
+
+/** 
+ * The PHPTemplate variant of the whole page templates 
+ */
+class PHPTemplatePageTemplate extends BasePageTemplate
+{
+    public function getTemplateString(): string
+    {
+        $renderedTitle = $this->titleTemplate->getTemplateString();
+
+        return <<<HTML
+        <div class="page"
+            $renderedTitle
+            <article class="content"><?= \$content; ?></article>
+        </div>
+        HTML;
+    }
+}
+
+/**
+ * The renderer is responsible for converting a template string into the actual
+ * HTML code. Each renderer behaves differently and expects its own type of
+ * template strings passed to it. Baking templates with the factory let you pass
+ * proper types of templates to proper renders.
+ */
+interface TemplateRenderer
+{
+    public function render(string $templateString, array $arguments = []): string;
+}
+
+/**
+ * The renderer for Twig templates.
+ */
+class TwigRenderer implements TemplateRenderer 
+{
+    public function render(string $templateString, array $arguments = []): string
+    {
+        return Twig::render($templateString, $arguments);
+    }
+}
+
+/**
+ * The renderer for PHPTemplate templates. Note that this implementation is very
+ * basic, if not crude. Using the `eval` function has many security
+ * implications, so use it with caution in real projects.
+ */
+class PHPTemplateRenderer implements TemplateRenderer
+{
+    public function render(string $templateString, array $arguments = []): string
+    {
+        extract($arguments);
+
+        ob_start();
+        eval(' ?>' . $templateString . '<?php ');
+        $result = ob_get_contents();
+        ob_end_clean();
+
+        return $result;
+    }
+}
+
+/**
+ * The client code. Note that it accepts the Abstract Factory class as the
+ * parameter, which allows the client to work with any concrete factory type.
+ */
+class Page 
+{
+    public $title, $content;
+
+    public function __construct($title, $content)
+    {
+        $this->title = $title;
+        $this->content = $content;
+    }
+
+    // Here's how you would use the template further in real life. Note that the
+    // page class does not depend on any contrete template classes.
+    public function render(TemplateFactory $factory): string
+    {
+        $pageTemplate = $factory->createPageTemplate();
+
+        $renderer = $factory->getRenderer();
+
+        return $renderer->render($pageTemplate->getTemplateString(), [
+            'title' => $this->title,
+            'content' => $this->content
+        ]);
+    }
+}
+
+/**
+ * Now, in other parts of the app, the client code can accept factory objects of
+ * any type.
+ */
+$page = new Page('Sample Page', 'This is the body.');
+
+echo "Testing actual rendering with the PHPTemplate factory: " . PHP_EOL;
+echo $page->render(new PHPTemplateFactory());
+
+echo PHP_EOL . PHP_EOL;
+
+/**
+ * Home-made Twig renderer class.
+ */
+class Twig
+{
+    public static function render(string $string, array $arguments = [])
+    {
+        return 'This is a Twig template: ' . PHP_EOL . $string . PHP_EOL;
+    }
+}
+
+
